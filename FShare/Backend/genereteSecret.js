e0bdada75e4dfab9e762811a3e79c0837730ba4e
Middlechild0 <jimmymathu28@gmail.com>
2024-09-27 13:51:29 +0300

    detect

--- FShare/Backend/genereteSecret.js
+++
@@ -1,8 +0,0 @@
-const { randomBytes } = require ('crypto');
-
-// Generate a random JWT secret
-const secret = randomBytes(64).toString('hex');
-console.log(secret);
-
-// Export the secret
-module.exports.JWT_secret = secret;
